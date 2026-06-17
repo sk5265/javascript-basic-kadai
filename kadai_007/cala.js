12371211934d599da6fc5c93f3bf55197cfd40b8
sk5265 <sk0704kmhr@gmail.com>
2026-06-17 23:31:38 +0900

    Add files via upload

---
+++ kadai_007/cala.js
@@ -0,0 +1,18 @@
+let num = 15;
+
+if (num%3 == 0) {
+    console.log ("３の倍数です");
+}
+
+if (num%5 == 0) {
+    console.log ("５の倍数です");
+}
+
+if (num%3 == 0 & num%5 == 0) {
+    console.log ("３の倍数と５の倍数です");
+}
+
+if(num%3 !== 0 & num%5 !== 0) {
+    console.log (num);
+}
+
